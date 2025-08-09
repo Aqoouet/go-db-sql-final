b1bf78477ea3b07f56241b587fb9f598af94f4c9
Aqouet <maximryazanoff@yandex.ru>
2025-08-09 21:02:08 +0300

    intermediate save

--- parcel.go
+++ parcel.go
@@ -15,8 +15,23 @@ func NewParcelStore(db *sql.DB) ParcelStore {
 func (s ParcelStore) Add(p Parcel) (int, error) {
 	// реализуйте добавление строки в таблицу parcel, используйте данные из переменной p
 
+    s, err := sql.Exec ("INSERT INTO parcel(client, status, address, created_at) VALUES (:client, :status, :address, :created_at)",
+        sql.Named("client", p.Client),
+        sql.Named("status", p.Status),
+        sql.Named("address", p.Address),
+        sql.Named("created_at", p.CreatedAt))
+
+    if err!=nil {
+        return 0, err
+    }
+
+    id, err := s.LastInsertID()
+    if err!= nil {
+        return 0, err
+    }
+
 	// верните идентификатор последней добавленной записи
-	return 0, nil
+	return id, nil
 }
 
 func (s ParcelStore) Get(number int) (Parcel, error) {
@@ -25,6 +40,15 @@ func (s ParcelStore) Get(number int) (Parcel, error) {
 
 	// заполните объект Parcel данными из таблицы
 	p := Parcel{}
+
+    row, err := s.QueryRow("SELECT * FROM parcel WHERE number = :number",
+        sql.Named("number",number))
+
+    err := row.Scan(&p.Number,&p.Client,&p.Status,&p.Address, &p.CreatedAt)
+
+    if err != nil {
+        return Parcel{}, err
+    }
 
 	return p, nil
 }
